130928b8ae0b4b088d702e3b6789e840fe2bd0cb
Yomi-Olugbodi Boluwatife <animecodelord@gmail.com>
2023-11-28 18:12:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./build/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    colors: {
+      papayawhip: {
+        light: "#fef4e4",
+        DEFAULT: "#ffefd5",
+        dark: "#fee5bc",
+      },
+    },
+    screens: {
+      widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+      tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
+    },
+    keyframes: {
+      "open-menu": {
+        "0%": { transform: "scaleY(0)" },
+        "80%": { transform: "scaleY(1.2)" },
+        "100%": { transform: "scaleY(1)" },
+      },
+    },
+    animation: {
+      "open-menu": "open-menu 0.5s ease-in-out forwards",
+    },
+  },
+};
+export const plugins = [];
